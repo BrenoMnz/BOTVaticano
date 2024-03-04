3ae47534569b0c23e29c953dd70e9b2b0f2e7300
Pedroooheg <pedrohenriquegomes2@hotmail.com>
2024-03-03 22:10:26 -0300

    teste push Pedro

--- BOTVaticano/Form1.Designer.cs
+++ BOTVaticano/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnOi = new System.Windows.Forms.Button();
             this.lblOi = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOi
@@ -50,11 +51,21 @@
             this.lblOi.Size = new System.Drawing.Size(0, 16);
             this.lblOi.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Teste Pedro";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblOi);
             this.Controls.Add(this.btnOi);
             this.Name = "Form1";
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button btnOi;
         private System.Windows.Forms.Label lblOi;
+        private System.Windows.Forms.Button button1;
     }
 }
 
