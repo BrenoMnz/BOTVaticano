7d5501ce82e07ef49c701f8c89e2c9d4a0eff3e4
Breno Menezes <breno.luizmenezes0123@gmail.com>
2024-05-04 21:34:16 -0300

    Classe carta adicionad

---
+++ BOTVaticano/Controllers/Carta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOTVaticano
+{
+    public class Carta
+    {
+        private int donoID;
+        private int valor;
+        private char naipe;
+
+        public int DonoID { get { return donoID; } set { donoID = value; } }
+        public char Naipe { get { return naipe; } set { naipe = value; } }
+        public int Valor { get { return valor; } set { valor = value; } }
+        public Carta(int idJogador, char naipe)
+        {
+            DonoID = idJogador;
+            Naipe = naipe;
+        }
+
+        public void atribuiValor(int valorDaCarta)
+        {
+            Valor = valorDaCarta;
+        }
+    }
+}
