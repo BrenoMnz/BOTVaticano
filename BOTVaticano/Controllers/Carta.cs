9957972acc2780df62bef5518c7f81ed95004e1d
Pedroooheg <pedrohenriquegomes2@hotmail.com>
2024-05-08 21:14:11 -0300

    Partida parcial

--- BOTVaticano/Controllers/Carta.cs
+++ BOTVaticano/Controllers/Carta.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BOTVaticano
 {
     public class Carta
     {
-        private int donoID;
+        private int idJogador;
         private int valor;
         private char naipe;
+        private int idCarta;
 
-        public int DonoID { get { return donoID; } set { donoID = value; } }
+        public int IdJogador { get { return idJogador; } set { idJogador = value; } }
         public char Naipe { get { return naipe; } set { naipe = value; } }
         public int Valor { get { return valor; } set { valor = value; } }
-        public Carta(int idJogador, char naipe)
+        public Carta(int idJogador, char naipe, int idCarta)
         {
-            DonoID = idJogador;
-            Naipe = naipe;
+            this.idJogador = idJogador;
+            this.naipe = naipe;
+            this.idCarta = idCarta; 
         }
 
         public void atribuiValor(int valorDaCarta)
---
+++ BOTVaticano/Controllers/Partida.cs
@@ -0,0 +1,112 @@
+﻿
+using BOTVaticano;
+using MagicTrickServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Partida
+{ 
+    private int idPartida;
+    private int qtdJogadores;
+    private int qtdCartas;
+    private int jogadorDaVez;
+    private int rodada;
+    private string[] vez;
+    
+    List<Carta> cartasRodada;
+
+
+    public Partida(int idPartida) 
+    {
+        this.idPartida = idPartida;
+        qtdJogadores = SepararJogadores().Length;
+
+        if (qtdJogadores == 2)
+        {
+            qtdCartas = 12;
+        }
+        if(qtdJogadores == 4) 
+        { 
+            qtdCartas = 14;
+        }
+
+        jogadorDaVez = -1;
+
+        cartasRodada = new List<Carta>();
+
+    }
+
+    public int QtdJogadores 
+    {
+        get 
+        { 
+            return qtdJogadores; 
+        }
+    }
+
+    public int QtdCartas
+    {
+        get
+        {
+            return qtdCartas;
+        }
+       
+    }
+    public int Rodada
+    {
+        get
+        {
+            return rodada;
+        }
+
+        set 
+        { 
+            rodada = Int32.Parse((vez[0].Split(','))[2]);
+        }
+
+    }
+
+    public int JogadorDaVez
+    {
+        get
+        {
+            return jogadorDaVez;
+        }
+
+        set
+        {
+            jogadorDaVez = Int32.Parse((vez[0].Split(','))[1]);
+        }
+
+    }
+    private string[] SepararJogadores()
+    {
+        int id = idPartida;
+        string listaJogadores = Jogo.ListarJogadores(id);
+        listaJogadores = listaJogadores.Replace("\r", "");
+        listaJogadores = listaJogadores.Substring(0, listaJogadores.Length - 1);
+        string[] jogadores = listaJogadores.Split('\n');
+        return jogadores;
+    }
+
+    void AtualizarVez() 
+    {
+        string checarVez = Jogo.VerificarVez2(idPartida);
+        checarVez = checarVez.Replace("\r", "");
+        string[] informacaoVez = checarVez.Split('\n');
+        informacaoVez = informacaoVez.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+        vez = informacaoVez;
+    }
+
+    void cartaJogada() 
+    {
+
+        //eseprando a mesaPartida
+
+    }
+
+
+
+    
+}
